a53b6ca4bb2d97b0b1cecf96daa19961b254cf16
mordilos <necktarios.black@gmail.com>
2023-06-13 17:22:53 +0300

    added the tests for the new endpoint

--- test/test_main.py
+++ test/test_main.py
@@ -8,7 +8,7 @@ input_file = "cvas_data.json"
 
 
 def test_index():
-    all_routes = ['/features', '/status', '/']
+    all_routes = ['/features_file', '/features_json', '/status', '/']
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"endpoints": all_routes}
@@ -20,13 +20,13 @@ def test_status():
     assert response.json() == {"status": "UP"}
 
 
-def test_create_features_no_params():
+def test_create_features_file_no_params():
     files = {"file": (input_file, open(input_file, "rb"))}
     data = {"feature_selection": []}
     test_response = open('test_response.json')
     test_response_json = json.dumps(json.load(test_response), indent=4)
 
-    response = client.post("/features",
+    response = client.post("/features_file",
                            files=files,
                            data=data)
 
@@ -34,49 +34,104 @@ def test_create_features_no_params():
     assert response.json() == test_response_json
 
 
-def test_create_features_highly_null():
+def test_create_features_file_highly_null():
     files = {"file": (input_file, open(input_file, "rb"))}
     data = {"feature_selection": ['highly_null_features']}
     test_response = open('test_response_highly_null.json')
     test_response_json = json.dumps(json.load(test_response), indent=4)
 
-    response = client.post("/features", files=files, data=data)
+    response = client.post("/features_file", files=files, data=data)
 
     assert response.status_code == 200
     assert response.json() == test_response_json
 
 
-def test_create_features_single_value():
+def test_create_features_file_single_value():
     files = {"file": (input_file, open(input_file, "rb"))}
     data = {"feature_selection": ['single_value_features']}
     test_response = open('test_response_single_value.json')
     test_response_json = json.dumps(json.load(test_response), indent=4)
 
-    response = client.post("/features", files=files, data=data)
+    response = client.post("/features_file", files=files, data=data)
 
     assert response.status_code == 200
     assert response.json() == test_response_json
 
 
-def test_create_features_highly_correlated():
+def test_create_features_file_highly_correlated():
     files = {"file": (input_file, open(input_file, "rb"))}
     data = {"feature_selection": ['highly_correlated_features']}
     test_response = open('test_response_highly_correlated.json')
     test_response_json = json.dumps(json.load(test_response), indent=4)
 
-    response = client.post("/features", files=files, data=data)
+    response = client.post("/features_file", files=files, data=data)
 
     assert response.status_code == 200
     assert response.json() == test_response_json
 
 
-def test_create_features_all():
+def test_create_features_file_all():
     files = {"file": (input_file, open(input_file, "rb"))}
     data = {"feature_selection": ['highly_null_features,single_value_features,highly_correlated_features']}
     test_response = open('test_response_all.json')
     test_response_json = json.dumps(json.load(test_response), indent=4)
 
-    response = client.post("/features", files=files, data=data)
+    response = client.post("/features_file", files=files, data=data)
+
+    assert response.status_code == 200
+    assert response.json() == test_response_json
+
+
+def test_create_features_json_no_params():
+    files = {"data": json.loads(open(input_file, "rb").read())['data'], "feature_selection": []}
+    test_response = open('test_response.json')
+    test_response_json = json.dumps(json.load(test_response), indent=4)
+
+    response = client.post("/features_json", json=files)
+
+    assert response.status_code == 200
+    assert response.json() == test_response_json
+
+
+def test_create_features_json_highly_null():
+    files = {"data": json.loads(open(input_file, "rb").read())['data'], "feature_selection": ['highly_null_features']}
+    test_response = open('test_response_highly_null.json')
+    test_response_json = json.dumps(json.load(test_response), indent=4)
+
+    response = client.post("/features_json", json=files)
+
+    assert response.status_code == 200
+    assert response.json() == test_response_json
+
+
+def test_create_features_json_single_value():
+    files = {"data": json.loads(open(input_file, "rb").read())['data'], "feature_selection": ['single_value_features']}
+    test_response = open('test_response_single_value.json')
+    test_response_json = json.dumps(json.load(test_response), indent=4)
+
+    response = client.post("/features_json", json=files)
+
+    assert response.status_code == 200
+    assert response.json() == test_response_json
+
+
+def test_create_features_json_highly_correlated():
+    files = {"data": json.loads(open(input_file, "rb").read())['data'], "feature_selection": ['highly_correlated_features']}
+    test_response = open('test_response_highly_correlated.json')
+    test_response_json = json.dumps(json.load(test_response), indent=4)
+
+    response = client.post("/features_json", json=files)
+
+    assert response.status_code == 200
+    assert response.json() == test_response_json
+
+
+def test_create_features_json_all():
+    files = {"data": json.loads(open(input_file, "rb").read())['data'], "feature_selection": ['highly_correlated_features','single_value_features', 'highly_correlated_features']}
+    test_response = open('test_response_all.json')
+    test_response_json = json.dumps(json.load(test_response), indent=4)
+
+    response = client.post("/features_json", json=files)
 
     assert response.status_code == 200
     assert response.json() == test_response_json
